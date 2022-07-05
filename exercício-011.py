104ac90d0acedf49e6199bab3762bfd459ec03b8
Bn Bonfim <bru.amorimbonfim@gmail.com>
2022-07-05 20:27:33 -0300

    Create exercício-011.py

---
+++ exercício-011.py
@@ -0,0 +1,6 @@
+la = float(input('Informe a largura:'))
+al = float(input('Informe a altura:'))
+a = la * al
+print('Sua parede tem a dimensão de {}x{} e sua área é de {}m².'.format(la, al, a))
+t = a / 2
+print('A quantidade de tinta necessária para pintar sua parede é:{}l de tinta.'.format(t))
